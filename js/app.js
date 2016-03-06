4f6be9f45be693cbac0120ec2f9e3f6cd9fac2ee
Sadhana <sadhana.varma029@gmail.com>
2016-03-06 18:56:44 +0530

    CF js version1

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
 $(document).foundation();
+$(document).ready(function(){
+  $('.menu').click(function(){
+   var id=$(this).attr("data-type");
+    $('.show').hide();
+    $('#'+id).show();
+  })
+})
