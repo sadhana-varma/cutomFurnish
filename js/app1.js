c5635a1bfb9788d1bd700d3a95ba3504e6abb066
Sadhana <sadhana@abhinavvarma.tk>
2016-03-14 11:10:55 +0530

    abhinav commit

--- js/app1.js
+++ js/app1.js
@@ -1,7 +1,4 @@
-/**
- * Created by Sadhana on 4/03/2016.
- */
-//$(document).foundation();
+
 var shape;
 $(document).ready(function(){
 
@@ -9,35 +6,116 @@ $(document).ready(function(){
      $('.main-top').attr("src","images/tables_data/tops/circle/36/top1/sheesham/40/natural2.png");
      $('.main-bottom').attr("src","images/tables_data/pedestals/circle/36/pedestal1/sheesham/natural2.jpg");
      shape='circle';
+     menu_click(shape);
+     $('.first-top-circle').click(function(){
+     $('.main-top').attr("src","images/tables_data/tops/circle/36/top1/sheesham/40/natural1.png");
+   })
+
+   $('.second-top-circle').click(function(){
+     $('.main-top').attr("src","images/tables_data/tops/circle/36/top1/sheesham/40/natural2.png");
+   })
+  $('.first-leg-circle').click(function(){
+   $('.main-bottom').attr("src","images/tables_data/pedestals/circle/36/pedestal1/sheesham/natural1.jpg");
+  })
+
+  $('.second-leg-circle').click(function(){
+    $('.main-bottom').attr("src","images/tables_data/pedestals/circle/36/pedestal1/sheesham/natural2.jpg");
+  })
+
  });
 
-    $('#rectangle').click(function(){
-      $('.main-top').attr("src","images/tables_data/tops/rectangle/4290/top1/sheesham/25/natural2.png");
-      $('.main-bottom').attr("src","images/tables_data/pedestals/rectangle/3072/pedestal2/sheesham/natural1.jpg");
-        shape='rectangle';
-    });
 
-    $('#square').click(function(){
+ $('#rectangle').click(function(){
+      $('.main-top').attr("src","images/tables_data/tops/rectangle/4290/top1/sheesham/25/natural2.png");
+      $('.main-bottom').attr("src","images/tables_data/pedestals/rectangle/3072/pedestal2/sheesham/natural2.jpg");
+      shape='rectangle';
+      menu_click(shape);
+     $('.first-top-rectangle').click(function(){
+     $('.main-top').attr("src","images/tables_data/tops/rectangle/4290/top1/sheesham/25/natural1.png");
+   })
+
+   $('.second-top-rectangle').click(function(){
+     $('.main-top').attr("src","images/tables_data/tops/rectangle/3072/top1/sheesham/25/natural2.png");
+   })
+
+  $('.first-leg-rectangle').click(function(){
+   $('.main-bottom').attr("src","images/tables_data/pedestals/rectangle/3072/pedestal2/sheesham/natural1.jpg");
+  })
+
+  $('.second-leg-rectangle').click(function(){
+    $('.main-bottom').attr("src","images/tables_data/pedestals/rectangle/3072/pedestal2/sheesham/natural2.jpg");
+  })
+
+ });
+
+ $('#square').click(function(){
      $('.main-top').attr("src","images/tables_data/tops/square/3636/top1/sheesham/25/natural2.png");
      $('.main-bottom').attr("src","images/tables_data/legs/square/3636/leg1/sheesham/natural2.jpg");
-        shape='square';
-    })
+     shape='square';
+     menu_click(shape);
+       $('.first-top-square').click(function(){
+     $('.main-top').attr("src","images/tables_data/tops/square/3636/top1/sheesham/25/natural1.png");
+   })
+  $('.second-top-square').click(function(){
+     $('.main-top').attr("src","images/tables_data/tops/square/3636/top1/sheesham/25/natural2.png");
+   })
+    $('.first-leg-square').click(function(){
+   $('.main-bottom').attr("src","images/tables_data/legs/square/3636/leg1/sheesham/natural2.jpg");
+  })
+
+  $('.second-leg-square').click(function(){
+    $('.main-bottom').attr("src","images/tables_data/legs/square/3636/leg1/sheesham/natural1.jpg");
+  })
+ });
+
+
+  $('.first-top').click(function(){
+     $('.main-top').attr("src","images/tables_data/tops/rectangle/3072/top1/sheesham/25/natural1.png");
+   })
+
+   $('.second-top').click(function(){
+     $('.main-top').attr("src","images/tables_data/tops/rectangle/3072/top1/sheesham/25/natural2.png");
+   })
+
+  $('.first-leg').click(function(){
+   $('.main-bottom').attr("src","images/tables_data/pedestals/rectangle/3072/pedestal2/sheesham/natural1.jpg");
+  })
+
+  $('.second-leg').click(function(){
+    $('.main-bottom').attr("src","images/tables_data/pedestals/rectangle/3072/pedestal2/sheesham/natural2.jpg");
+  })
+
+
+ function menu_click(x){
+   var shape=x;
+   $('.menu').click(function(){
+    var id=$(this).attr("data-type");
+    if(id!='shapes')
+    {
+      $('.show').hide();
+      $('.'+ shape+'-'+id).show();
+      $('.'+id).click(function(){
+      var size=$(this).attr("data-size");
+      var source=$('.'+id+'-'+size).attr('src');
+      if(id=='sizes')
+       $('.main-top').attr("src",source);
+      else
+       $('.main-bottom').attr("src",source);
+      if(id=='top'||'leg')
+      {
+
+      }
+
+      })
+    }
+   })
+ }
+
 
   $('.menu').click(function(){
     var id=$(this).attr("data-type");
        $('.show').hide();
        $('.'+id).show();
-        $('.'+id).click(function() {
-            if (id == 'shapes')
-            {
-                shape = $(this).attr("data-shape");
-
-            }
-
-        })
     })
-
-
-  alert(shape);
-})
+  })
 
