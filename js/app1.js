8b7d8f9ffbb5e81f4ad3ab6aa8e50f5b6fc729fb
Sadhana <sadhana.varma029@gmail.com>
2016-03-16 11:00:53 +0530

    json added version 1

--- js/app1.js
+++ js/app1.js
@@ -1,93 +1,9 @@
 
 var shape;
 $(document).ready(function(){
-
- $('#circle').click(function() {
-     $('.main-top').attr("src","images/tables_data/tops/circle/36/top1/sheesham/40/natural2.png");
-     $('.main-bottom').attr("src","images/tables_data/pedestals/circle/36/pedestal1/sheesham/natural2.jpg");
-     shape='circle';
-     menu_click(shape);
-     $('.first-top-circle').click(function(){
-     $('.main-top').attr("src","images/tables_data/tops/circle/36/top1/sheesham/40/natural1.png");
-   })
-
-   $('.second-top-circle').click(function(){
-     $('.main-top').attr("src","images/tables_data/tops/circle/36/top1/sheesham/40/natural2.png");
-   })
-  $('.first-leg-circle').click(function(){
-   $('.main-bottom').attr("src","images/tables_data/pedestals/circle/36/pedestal1/sheesham/natural1.jpg");
-  })
-
-  $('.second-leg-circle').click(function(){
-    $('.main-bottom').attr("src","images/tables_data/pedestals/circle/36/pedestal1/sheesham/natural2.jpg");
-  })
-
- });
-
-
- $('#rectangle').click(function(){
-      $('.main-top').attr("src","images/tables_data/tops/rectangle/4290/top1/sheesham/25/natural2.png");
-      $('.main-bottom').attr("src","images/tables_data/pedestals/rectangle/3072/pedestal2/sheesham/natural2.jpg");
-      shape='rectangle';
-      menu_click(shape);
-     $('.first-top-rectangle').click(function(){
-     $('.main-top').attr("src","images/tables_data/tops/rectangle/4290/top1/sheesham/25/natural1.png");
-   })
-
-   $('.second-top-rectangle').click(function(){
-     $('.main-top').attr("src","images/tables_data/tops/rectangle/3072/top1/sheesham/25/natural2.png");
-   })
-
-  $('.first-leg-rectangle').click(function(){
-   $('.main-bottom').attr("src","images/tables_data/pedestals/rectangle/3072/pedestal2/sheesham/natural1.jpg");
-  })
-
-  $('.second-leg-rectangle').click(function(){
-    $('.main-bottom').attr("src","images/tables_data/pedestals/rectangle/3072/pedestal2/sheesham/natural2.jpg");
-  })
-
- });
-
- $('#square').click(function(){
-     $('.main-top').attr("src","images/tables_data/tops/square/3636/top1/sheesham/25/natural2.png");
-     $('.main-bottom').attr("src","images/tables_data/legs/square/3636/leg1/sheesham/natural2.jpg");
-     shape='square';
-     menu_click(shape);
-       $('.first-top-square').click(function(){
-     $('.main-top').attr("src","images/tables_data/tops/square/3636/top1/sheesham/25/natural1.png");
-   })
-  $('.second-top-square').click(function(){
-     $('.main-top').attr("src","images/tables_data/tops/square/3636/top1/sheesham/25/natural2.png");
-   })
-    $('.first-leg-square').click(function(){
-   $('.main-bottom').attr("src","images/tables_data/legs/square/3636/leg1/sheesham/natural2.jpg");
-  })
-
-  $('.second-leg-square').click(function(){
-    $('.main-bottom').attr("src","images/tables_data/legs/square/3636/leg1/sheesham/natural1.jpg");
-  })
- });
-
-
-  $('.first-top').click(function(){
-     $('.main-top').attr("src","images/tables_data/tops/rectangle/3072/top1/sheesham/25/natural1.png");
-   })
-
-   $('.second-top').click(function(){
-     $('.main-top').attr("src","images/tables_data/tops/rectangle/3072/top1/sheesham/25/natural2.png");
-   })
-
-  $('.first-leg').click(function(){
-   $('.main-bottom').attr("src","images/tables_data/pedestals/rectangle/3072/pedestal2/sheesham/natural1.jpg");
-  })
-
-  $('.second-leg').click(function(){
-    $('.main-bottom').attr("src","images/tables_data/pedestals/rectangle/3072/pedestal2/sheesham/natural2.jpg");
-  })
-
-
  function menu_click(x){
    var shape=x;
+   var source,value;
    $('.menu').click(function(){
     var id=$(this).attr("data-type");
     if(id!='shapes')
@@ -95,41 +11,165 @@ $(document).ready(function(){
       $('.show').hide();
       $('.'+ shape+'-'+id).show();
       $('.'+id).click(function(){
-      if(id!='top'||'leg')
-      {
+      //if(id!='top'||'leg')
+      //{
       var size=$(this).attr("data-size");
-      var source=$('.'+id+'-'+size).attr('src');
+      var num=$(this).attr("data-num");
+      var sheesham='sheesham',natural1='natural1',natural2='natural2';
+       if(id!='sizes')
+       source=tables_data[id][shape][size][num][sheesham][natural1];
+       else{
+           if(shape=='circle')
+             value=40;
+           else
+             value=25;
+        source=tables_data[id][shape][size][num][sheesham][value][natural1];
+       }
+
+       //var source=$('.'+id+'-'+size).attr('src');
       if(id=='sizes')
        $('.main-top').attr("src",source);
       else
        $('.main-bottom').attr("src",source);
-      }
-      else
-      {
-          if(id=='top'){
-              $('.first-top').click(function(){
-                  var first='first',top='top';
-                  var source=$('.'+id+'-'+size+'-'+first+'-'+top).attr('src');
-                   $('.main-top').attr("src",source);
-              })
+      //}
+      //else
+      //{
+      //    if(id=='top'){
+      //        $('.first-top').click(function(){
+      //            var first='first',top='top';
+      //            var source=$('.'+id+'-'+size+'-'+first+'-'+top).attr('src');
+      //             $('.main-top').attr("src",source);
+      //        })
+      //
+      //        $('.second-top').click(function(){
+      //            var source=$('.'+id+'-'+size).attr('src');
+      //             $('.main-bottom').attr("src",source);
+      //        })
+      //    }
+      //}
+      })
+    }
+    else{
+        $('.'+id).show();
+        $('.'+id).click(function(){
 
-              $('.second-top').click(function(){
-                  var source=$('.'+id+'-'+size).attr('src');
-                   $('.main-bottom').attr("src",source);
+        })
+    }
+   })
+ }
+
+
+  //
+  //$('.menu').click(function(){
+  //  var id=$(this).attr("data-type");
+  //     $('.show').hide();
+  //     $('.'+id).show();
+  //  })
+  })
+
+$('.shapes').click(function(){
+    var shape=$(this).attr("data-shape");
+    var size=$(this).attr("data-size");
+    var value=$(this).attr("data-value");
+    var bottom=$(this).attr("data-bottom");
+    var num=$(this).attr("data-num");
+    var sheesham='sheesham',natural1='natural1',natural2='natural2',sizes='sizes',top1='top1';
+
+    var topsource=tables_data[sizes][shape][size][top1][sheesham][value][natural2];
+    var bottomsource=tables_data[bottom][shape][size][num][sheesham][natural2];
+
+    $('.main-top').attr("src",topsource);
+    $('.main-bottom').attr("src",bottomsource);
+
+    $('.top').click(function(){
+        var natural= $(this).attr('data-type');
+        var topsource=tables_data[sizes][shape][size][top1][sheesham][value][natural];
+        $('.main-top').attr("src",topsource);
+    })
+    $('.leg').click(function(){
+        var natural= $(this).attr('data-type');
+        var bottomsource=tables_data[bottom][shape][size][num][sheesham][natural];
+        $('.main-bottom').attr("src",bottomsource);
+    })
+    menu_click(shape);
+})
+
+//function toplegclick(x,y,a,b,c,d,id){
+//    var shape= x,natural= y,size= a,value= b,bottom= c,num= d,id=id,sizes='sizes';
+//    if(id=='top')
+//    {
+//         var topsource=tables_data[sizes][shape][size]['top1']['sheesham'][value][natural];
+//         $('.main-top').attr("src",topsource);
+//    }
+//    else
+//    {
+//        var bottomsource=tables_data[bottom][shape][size][num]['sheesham'][natural];
+//        $('.main-bottom').attr("src",bottomsource);
+//    }
+//}
+
+$('.top').click(function(){
+    var natural= $(this).attr('data-type'),shape='rectangle',sizes='sizes',top1='top1',size='3048',sheesham='sheesham',value='25';
+    var topsource=tables_data[sizes][shape][size][top1][sheesham][value][natural];
+     $('.main-top').attr("src",topsource);
+})
+$('.leg').click(function(){
+    var natural= $(this).attr('data-type'),shape='rectangle',sizes='sizes',top1='top1',size='3048',sheesham='sheesham',value='25';
+    var bottomsource=tables_data[sizes][shape][size][top1][sheesham][value][natural];
+    $('.main-bottom').attr("src",bottomsource);
+})
+var x ='rectangle';
+menu_click(x);
+
+function menu_click(x){
+   var shape= x,source,value;
+   $('.menu').click(function(){
+    var id=$(this).attr("data-type");
+     //if(id=='top'||'leg')
+     //{
+     //    var natural=$(this).attr("data-type");
+     //    toplegclick(shape,natural,a,b,c,d,id);
+     //}
+     //
+    if((id!='shapes'))
+    {
+      $('.show').hide();
+      $('.'+ shape+'-'+id).show();
+      $('.'+id).click(function(){
+        if(id!='top'||'leg') {
+            var size = $(this).attr("data-size");
+            var num = $(this).attr("data-num");
+            var sheesham = 'sheesham', natural1 = 'natural1', natural2 = 'natural2';
+            if (id != 'sizes') {
+                source = tables_data[id][shape][size][num][sheesham][natural2];
+                $('.main-bottom').attr("src", source);
+            }
+            else {
+                if (shape == 'circle')
+                    value = 40;
+                else
+                    value = 25;
+                source = tables_data[id][shape][size][num][sheesham][value][natural2];
+                $('.main-top').attr("src", source);
+            }
+       }
+       else
+      {
+          if(id=='top')
+          {
+              $('.top').click(function(){
+                   var natural= $(this).attr('data-type');
+                 source=tables_data[id][shape][size][num][sheesham][value][natural];
+                 $('.main-top').attr("src",source);
               })
+          }
+          else {
+               var natural= $(this).attr('data-type');
+           source=tables_data[id][shape][size][num][sheesham][natural];
+           $('.main-bottom').attr("src",source);
           }
       }
       })
     }
    })
  }
-
-
-
-  $('.menu').click(function(){
-    var id=$(this).attr("data-type");
-       $('.show').hide();
-       $('.'+id).show();
-    })
-  })
-
